4ae492ab9c45200144f382212c9ef982922b99cc
Bit-edu <166929960+Bit-edu@users.noreply.github.com>
2024-05-05 15:25:27 +0900

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -50,7 +50,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     const char* pszTimestamp = "The Times 24/Jan/2024 International Day of Education";
-    const CScript genesisOutputScript = CScript() << ParseHex("0435571cf46983462367e8a04d9d35193ee7ce4e1d2189805629d797f35d026089b14ad003aa5962fdfad99b6fc67e04297be9e87443d0691d768568b016bb2db4") << OP_CHECKSIG;
+    const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
